3b318066b32585c74562d26dd65753678725612d
Ruben <tao_tyy@sina.com>
2021-03-18 11:57:23 +0800

    创建项目

---
+++ src/test/java/edu/bistu/rojserver/RubenOjServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bistu.rojserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubenOjServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
