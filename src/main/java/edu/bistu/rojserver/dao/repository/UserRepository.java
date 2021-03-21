d7e99e9bdb64e01edd48b4b2f7d9bc7e4ddbfdc3
ruben <tao_tyy@sina.com>
2021-03-21 15:49:30 +0800

    veloping user module.

---
+++ src/main/java/edu/bistu/rojserver/dao/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package edu.bistu.rojserver.dao.repository;
+
+import edu.bistu.rojserver.dao.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long>
+{
+    Optional<UserEntity> findUserEntityByUsernameAndPassword(String username, String password);
+}
